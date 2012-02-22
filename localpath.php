dfe225e2d27aed17d5bad325bf8756c9a8bbc7d9
bshelton229 <bshelton229@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-22 20:51:44 +0000

    Bumped to v0.4
    git-svn-id: https://plugins.svn.wordpress.org/localpath/trunk@509038 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- localpath.php
+++ localpath.php
@@ -7,7 +7,7 @@
 Plugin Name: LocalPath
 Description: Re-write URLs away from admin URLs to the current host.
 Author: University Communications at the University of Wisconsin-Madison
-Version: 0.3
+Version: 0.4
 */
 
 require_once dirname(__FILE__) . '/lib/localpath.class.php';
